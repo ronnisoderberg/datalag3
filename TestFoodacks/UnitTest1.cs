1d558bd443c708d8d286f9169b6d8e3a398ddb25
ronni söderberg <ronni.soderberg@gmail.com>
2021-11-27 21:45:20 +0100

    start and seeds database

--- TestFoodacks/UnitTest1.cs
+++ TestFoodacks/UnitTest1.cs
@@ -1,9 +1,22 @@
 using Xunit;
+using DataLayer;
+using DataLayer.Backend;
+using DataLayer.Data;
+using DataLayer.Model;
 
 namespace TestFoodacks
 {
     public class UnitTest1
     {
+        public UnitTest1()
+        {
+            var startNseed = new AdminBackend();
+            startNseed.PrepDatabase();
+            
+
+
+
+        }
         [Fact]
         public void Test1()
         {
